0ca5990c0e36071dfc7f0a4053b0fbae8c14889d
marcusjacobsen <S215799@dtu.dk>
2023-02-13 16:55:52 +0100

    Changes to

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
+import {FormEvent, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+function App() {
+    return(
+        <div className="App">
+            <FetchProducts />
+        </div>
+    )
+}
+
+function FetchProducts() {
+    const [ids, setProducts] = useState<string[]>([]);
+    async function handleSubmit(e:FormEvent) {
+        e.preventDefault();
+        const URL = "https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json";
+        const response = await fetch(URL);
+        const jsonProducts = (await response.json()) as {
+            products: { id: string, name: string, price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: String }[];
+        };
+        const {products} = jsonProducts
+
+        setProducts(products.map(({id}) => id));
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <button type="submit">GO!</button>
+            <p>Product id's:</p>
+            <ol>
+                {ids.map((id)=>(
+                    <li>{id}</li>
+                ))}
+            </ol>
+        </form>
+
+    )
 }
 
 export default App
