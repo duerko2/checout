b555460d656156fd72fbc0f97e084c6fbbebe201
marcusjacobsen <S215799@dtu.dk>
2023-02-13 22:57:19 +0100

    Nu viser den alt det der fra json. Der er også knapper til + og - units, men der sker ikke noget

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import {FormEvent, useState} from 'react'
-import reactLogo from './assets/react.svg'
+import {useState} from 'react'
 import './App.css'
+import React from 'react';
 
 
 
@@ -12,29 +12,56 @@ function App() {
     )
 }
 
-function FetchProducts() {
-    const [ids, setProducts] = useState<string[]>([]);
-    async function handleSubmit(e:FormEvent) {
-        e.preventDefault();
-        const URL = "https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json";
-        const response = await fetch(URL);
-        const jsonProducts = (await response.json()) as {
-            products: { id: string, name: string, price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: String }[];
-        };
-        const {products} = jsonProducts
 
-        setProducts(products.map(({id}) => id));
+
+function FetchProducts() {
+    const [products, setProducts] = useState<{id: string, name: string,price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: string}[]>([]);
+    async function onClick() {
+        const URL = "https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json";
+        try {
+            const response = await fetch(URL);
+            const result = (await response.json()) as {id: string, name: string, price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: string}[];
+
+            let products : {id: string, name: string, price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: string}[] = [];
+            for(let i=0;i<result.length;i++){
+                products.push(result[i])
+            }
+            setProducts(products.map(e =>e));
+
+        } catch(e){
+            console.log(e)
+        }
     }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <button type="submit">GO!</button>
-            <p>Product id's:</p>
-            <ol>
-                {ids.map((id)=>(
-                    <li>{id}</li>
+        <div>
+            <button onClick={onClick}>GO!</button>
+            <p>Basket</p>
+                {products.map((product)=>(
+                    <div key={product.id} className="product-grid">
+
+                        <div className="grid-title">Product ID</div>
+                        <div className="grid-title">Product Name</div>
+                        <div className="grid-title">Unit Price</div>
+                        <div className="grid-title">Discount</div>
+                        <div className="grid-title">Units</div>
+                        <div className="grid-title">Total Price</div>
+
+                        <div className="grid-item">{product.id}</div>
+                        <div className="grid-item">{product.name}</div>
+                        <div className="grid-item">{product.price}</div>
+                        <div className="grid-item">{product.rebatePercent}%</div>
+                        <div className="grid-item">
+                            <button className="unit-button">-</button>
+                            <a>{product.rebateQuantity}</a>
+                            <button className="unit-button">+</button>
+                        </div>
+                        <div className="grid-item">{product.price*product.rebatePercent*product.rebateQuantity}</div>
+
+                    </div>
                 ))}
-            </ol>
-        </form>
+
+        </div>
 
     )
 }
