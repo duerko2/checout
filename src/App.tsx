3121d768ec2d71728ab8d433273159f36d185b91
marcusjacobsen <S215799@dtu.dk>
2023-02-14 21:17:22 +0100

    gift wrap toggle and delete row

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import './App.css'
 import React from 'react';
+import minus from '/assets/minus-png.png'
+
 
 
 
@@ -11,21 +13,29 @@ function App() {
         </div>
     )
 }
+type Product = {
+    id: string;
+    name: string;
+    price: number;
+    currency: string;
+    rebateQuantity: number;
+    rebatePercent: number;
+    upsellProductId: string;
+}
+type Item ={ product: Product; quantity: number; giftWrap: boolean }
 
 function Basket() {
-    const [products, setProducts] = useState<{id: string, name: string,price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: string}[]>([]);
-    fetchProducts();
+    const [itemList,setItems] = useState<Item[]>([])
+    if(!itemList[0]){fetchProducts();}
     async function fetchProducts() {
         const URL = "https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json";
         try {
             const response = await fetch(URL);
-            const result = (await response.json()) as {id: string, name: string, price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: string}[];
+            const result = (await response.json()) as Product[];
 
-            let products : {id: string, name: string, price: number, currency: String, rebateQuantity: number, rebatePercent: number, upsellProductId: string}[] = [];
-            for(let i=0;i<result.length;i++){
-                products.push(result[i])
-            }
-            setProducts(products.map(e =>e));
+            setItems(result.map(
+                (p) => ({product: p,quantity:0,giftWrap:false})
+            ));
 
         } catch(e){
             console.log(e)
@@ -35,67 +45,98 @@ function Basket() {
 
     }
 
-    function less(product: { id: string; name: string; price: number; currency: String; rebateQuantity: number; rebatePercent: number; upsellProductId: string }) {
-
-        let newProducts=products.map(e=>e);
-        for(let i=0;i<products.length;i++){
-            if(products[i].rebateQuantity===0){
-            } else if(products[i].id===product.id){
-                newProducts[i].rebateQuantity--;
+    function less(item : Item) {
+        let newItems=itemList.map(e=>e);
+        for(let i=0;i<itemList.length;i++){
+            if(itemList[i].product.id===item.product.id && itemList[i].quantity>0){
+                newItems[i].quantity--;
             }
         }
-        setProducts(newProducts);
+        setItems(newItems);
     }
 
-    function more(product: { id: string; name: string; price: number; currency: String; rebateQuantity: number; rebatePercent: number; upsellProductId: string }) {
-        let newProducts=products.map(e=>e);
-        for(let i=0;i<products.length;i++){
-            if(products[i].id===product.id){
-                newProducts[i].rebateQuantity++;
+    function more(item : Item) {
+        let newItems=itemList.map(e=>e);
+        for(let i=0;i<itemList.length;i++){
+            if(itemList[i].product.id===item.product.id){
+                newItems[i].quantity++;
             }
         }
-        setProducts(newProducts);
+        setItems(newItems);
     }
 
     function getTotal() {
         let total : number = 0;
-        products.forEach(p=>total+=p.price*(1-p.rebatePercent*(1/100))*p.rebateQuantity);
+        itemList.forEach(p=>total+=p.product.price*(1-p.product.rebatePercent*(1/100))*p.quantity);
         return total;
+    }
+
+    function calculateRebate(item: Item) {
+        if(item.quantity>=item.product.rebateQuantity && item.product.rebateQuantity!=0){
+            return item.product.rebatePercent;
+        } else return 0;
+    }
+
+    function changeGiftWrapped(item: Item) {
+        let newItems=itemList.map(e=>e);
+        for(let i=0;i<itemList.length;i++){
+            if(itemList[i].product.id===item.product.id){
+                newItems[i].giftWrap=!newItems[i].giftWrap
+            }
+        }
+        setItems(newItems);
+    }
+
+    function removeItem(item: Item) {
+        let newItems=itemList.map(e=>e);
+        for(let i=0;i<itemList.length;i++){
+            if(itemList[i].product.id===item.product.id){
+                newItems.splice(i,i);
+            }
+        }
+        setItems(newItems);
     }
 
     return (
         <div>
             <p>Basket</p>
-                {products.map((product)=>(
-                    <div key={product.id} className="product-grid">
+            <div className="product-grid">
+                <div></div>
+                <div className="grid-title">Product ID</div>
+                <div className="grid-title">Product Name</div>
+                <div className="grid-title">Unit Price</div>
+                <div className="grid-title">Discount</div>
+                <div className="grid-title">Units</div>
+                <div className="grid-title">Total Price</div>
+                <div className="grid-title">Gift Wrapped</div>
 
-                        <div className="grid-title">Product ID</div>
-                        <div className="grid-title">Product Name</div>
-                        <div className="grid-title">Unit Price</div>
-                        <div className="grid-title">Discount</div>
-                        <div className="grid-title">Units</div>
-                        <div className="grid-title">Total Price</div>
-
-                        <div className="grid-item">{product.id}</div>
-                        <div className="grid-item">{product.name}</div>
-                        <div className="grid-item">{product.price} {product.currency}</div>
-                        <div className="grid-item">{product.rebatePercent}%</div>
-                        <div className="grid-item">
-                            <button className="unit-button" onClick={ () => less(product) }>-</button>
-                            <a>{product.rebateQuantity}</a>
-                            <button className="unit-button" onClick={ () => more(product) }>+</button>
+                {itemList.map((item)=>(
+                    <>
+                        <div>
+                            <img src={minus} onClick={()=>removeItem(item)}/>
                         </div>
-                        <div className="grid-item">{(product.price*(1-product.rebatePercent*(1/100))*product.rebateQuantity).toFixed(2)} {product.currency}</div>
-
-                    </div>
-                ))}
-            <div className="grand-total">
-                <p>GRAND TOTAL: {getTotal().toFixed(2) } {products[0]?.currency}</p>
+                        <div className="grid-item">{item.product.id}</div>
+                        <div className="grid-item">{item.product.name}</div>
+                        <div className="grid-item">{item.product.price} {item.product.currency}</div>
+                        <div className="grid-item">{calculateRebate(item)}%</div>
+                        <div className="grid-item">
+                            <button className="unit-button" onClick={() => less(item)}>-</button>
+                            <a>{item.quantity}</a>
+                            <button className="unit-button" onClick={() => more(item)}>+</button>
+                        </div>
+                        <div className="grid-item">{(item.product.price * (1 - calculateRebate(item) * (1 / 100)) * item.quantity).toFixed(2)} {item.product.currency}</div>
+                        <div className="grid-item">
+                            <label>
+                                <input type="checkbox" onChange={()=>changeGiftWrapped(item)}/>
+                            </label>
+                        </div>
+                    </>
+                    ))}
             </div>
-
-
+            <div className="grand-total">
+                <p>GRAND TOTAL: {getTotal().toFixed(2) } {itemList[0]?.product.currency}</p>
+            </div>
         </div>
-
     )
 }
 
