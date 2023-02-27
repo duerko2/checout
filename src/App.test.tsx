56520d50113b9bc422052340e30f6805a59f04b1
marcusjacobsen <S215799@dtu.dk>
2023-02-27 15:19:21 +0100

    Final commit før første aflevering. Logo og titel tilføjet Første test er med Monthly recurring er blevet lavet til state

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import React from "react";
+import App from "./App";
+
+
+describe(App.name, () => {
+    it("should render", () => {
+        render(<App/>)
+        expect(screen.getByText("GRAND TOTAL: 0.00")).toBeInTheDocument();
+    });
+});
+
