be7672029ac5838752432cb3038c33ab65b0cccc
marcusjacobsen <S215799@dtu.dk>
2023-03-12 22:56:51 +0100

    Personal forms

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,104 @@ import question from './assets/question-mark.png';
 
 function App() {
     return(
-        <div className="basket">
-            <Basket />
+        <div className="page-grid">
+            <div className="basket">
+                <Basket />
+            </div>
+            <div className="delivery">
+                <Delivery/>
+            </div>
         </div>
     )
 }
-function BasketGrid({itemList,setItems}: {itemList:Item[],setItems:(items:Item[])=>void}) {
 
+function Basket() {
+    let products: { [id: string] : Product } = {};
+    const [loaded,setLoaded] = useState<Boolean>(false);
     const [show,setShowRebate] = useState<{ showRebate:boolean; product?:Product;pos:{x:number;y:number} }>({showRebate:false,product:undefined,pos:{x:0,y:0}});
+    const [itemList,setItems] = useState<Item[]>([]);
+    const [order,setOrder] = useState<Order>({itemList:itemList,recurring:false});
+
+
+
+    if(!loaded) {
+        fetchProducts().then(fetchBasket);
+        setLoaded(true)
+    }
+
+    async function fetchProducts() {
+        const URL = "https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json";
+        try {
+            const response = await fetch(URL);
+            const result = (await response.json()) as Product[];
+            result.map(
+                (p) => (products[p.id] = p)
+            );
+        } catch(e){
+            console.log(e)
+        }
+    }
+    async function fetchBasket() {
+        // TODO: async kald til backend for at hente indkøbskurv
+
+        let basket = [
+            { product: products["coffeebeans-organic-500g"], quantity: 1, giftWrap: false },
+            { product: products["coffee-grinder-pro"], quantity: 1, giftWrap: false },
+            { product: products["coffeebeans-500g"], quantity: 1, giftWrap: false },
+
+        ];
+        setItems(basket);
+    }
+    function getTotal() {
+        let total : number = 0;
+        itemList.forEach(p=>total+=p.product.price*(1-p.product.rebatePercent*(1/100))*p.quantity);
+        if(total>300){
+            total = total*0.9;
+        }
+        return total;
+    }
+    function changeRecurringOrder() {
+        if (order.recurring){
+            setOrder({itemList:itemList,recurring:false})
+        } else {
+            setOrder({itemList:itemList,recurring:true})
+        }
+    }
+    if(itemList.length===0){
+        return <div>
+            <p>Basket is empty</p>
+        </div>
+    } else return (
+        <div>
+            <h2>Basket</h2>
+            <BasketGrid
+                itemList={itemList}
+                setItems={setItems}
+                show={show}
+                setShowRebate={setShowRebate}
+            />
+            <div className="grand-total" >
+                <h2>GRAND TOTAL: {getTotal().toFixed(2) } {itemList[0]?.product.currency}</h2>
+            </div>
+
+            <div className="recurring-order">
+                <label>
+                    <h2>Monthly recurring order: <input type="checkbox" onChange={()=>changeRecurringOrder()}/></h2>
+                </label>
+            </div>
+
+            <div>
+                <ShowRebate
+                 showRebate={show.showRebate}
+                 product={show.product}
+                 pos={show.pos}
+                />
+            </div>
+        </div>
+    )
+}
+
+function BasketGrid({itemList,setItems,show,setShowRebate}: {itemList:Item[],setItems:(items:Item[])=>void,show:{ showRebate:boolean; product?:Product;pos:{x:number;y:number} },setShowRebate:(show:{ showRebate:boolean; product?:Product;pos:{x:number;y:number} })=>void}) {
 
     function calculateRebate(item: Item) {
         if(item.quantity>=item.product.rebateQuantity && item.product.rebateQuantity!=0){
@@ -82,138 +172,45 @@ function BasketGrid({itemList,setItems}: {itemList:Item[],setItems:(items:Item[]
     }
 
     return (<div>
-        <div className="product-grid">
-            <div className="grid-title"></div>
-            <div className="grid-title">Product Name</div>
-            <div className="grid-title">Unit Price</div>
-            <div className="grid-title">Discount</div>
-            <div className="grid-title">Units</div>
-            <div className="grid-title">Total Price</div>
-            <div className="grid-title">Gift Wrapped</div>
-        </div>
-    {itemList.map((item)=>(
-        <div className="product-card">
             <div className="product-grid">
-                <div className="grid-item" id="minus-thing">
-                    <button onClick={()=>removeItem(item)} className="minus-button"><img src={minus} height="25" width="25"/></button>
-                </div>
-                <div className="grid-item">{item.product.name}</div>
-                <div className="grid-item">{item.product.price} {item.product.currency}</div>
-                <div className="grid-item">{calculateRebate(item)}%
-                    <div className="rebate-question" onMouseEnter={(event)=>showRebateItem(item,event)} onMouseLeave={(event)=>unshowRebateItem(event)}> <img src={question} className="question-img"/></div>
-
-                </div>
-                <div className="grid-item">
-                    <button className="unit-button" onClick={() => less(item)}>-</button>
-                    <a>{item.quantity}</a>
-                    <button className="unit-button" onClick={() => more(item)}>+</button>
-                </div>
-                <div className="grid-item">{(item.product.price * (1 - calculateRebate(item) * (1 / 100)) * item.quantity).toFixed(2)} {item.product.currency}</div>
-                <div className="grid-item" style={{placeSelf:"center"}}>
-                    <label>
-                        <input type ="checkbox" onChange={()=>changeGiftWrapped(item)}/>
-                    </label>
-                </div>
+                <div className="grid-title"></div>
+                <div className="grid-title">Product Name</div>
+                <div className="grid-title">Unit Price</div>
+                <div className="grid-title">Discount</div>
+                <div className="grid-title">Units</div>
+                <div className="grid-title">Total Price</div>
+                <div className="grid-title">Gift Wrapped</div>
             </div>
-        </div>
-    ))}
-        </div>
-    )
-}
+            {itemList.map((item)=>(
+                <div className="product-card">
+                    <div className="product-grid">
+                        <div className="grid-item" id="minus-thing">
+                            <button onClick={()=>removeItem(item)} className="minus-button"><img src={minus} height="25" width="25"/></button>
+                        </div>
+                        <div className="grid-item">{item.product.name}</div>
+                        <div className="grid-item">{item.product.price} {item.product.currency}</div>
+                        <div className="grid-item">{calculateRebate(item)}%
+                            <div className="rebate-question" onMouseEnter={(event)=>showRebateItem(item,event)} onMouseLeave={(event)=>unshowRebateItem(event)}> <img src={question} className="question-img"/></div>
 
-function Basket() {
-    let products: { [id: string] : Product } = {};
-    const [loaded,setLoaded] = useState<Boolean>(false);
-    const [show,setShowRebate] = useState<{ showRebate:boolean; product?:Product;pos:{x:number;y:number} }>({showRebate:false,product:undefined,pos:{x:0,y:0}});
-    const [itemList,setItems] = useState<Item[]>([]);
-    const [order,setOrder] = useState<Order>({itemList:itemList,recurring:false});
-    let recurring = false;
-
-
-
-    if(!loaded) {
-        fetchProducts().then(fetchBasket);
-        setLoaded(true)
-    }
-
-    async function fetchProducts() {
-        const URL = "https://raw.githubusercontent.com/larsthorup/checkout-data/main/product.json";
-        try {
-            const response = await fetch(URL);
-            const result = (await response.json()) as Product[];
-
-
-
-            result.map(
-                (p) => (products[p.id] = p)
-            );
-
-
-        } catch(e){
-            console.log(e)
-        }
-    }
-    async function fetchBasket() {
-        // TODO: async kald til backend for at hente indkøbskurv
-
-        let basket = [
-            { product: products["coffeebeans-organic-500g"], quantity: 1, giftWrap: false },
-            { product: products["coffee-grinder-pro"], quantity: 1, giftWrap: false },
-            { product: products["coffeebeans-500g"], quantity: 1, giftWrap: false },
-        ];
-        setItems(basket);
-    }
-
-
-
-    function getTotal() {
-        let total : number = 0;
-        itemList.forEach(p=>total+=p.product.price*(1-p.product.rebatePercent*(1/100))*p.quantity);
-        return total;
-    }
-
-
-
-    function changeRecurringOrder() {
-        if (order.recurring){
-            setOrder({itemList:itemList,recurring:false})
-        } else {
-            setOrder({itemList:itemList,recurring:true})
-        }
-    }
-
-
-
-
-
-    return (
-        <div>
-            <h2>Basket</h2>
-            <BasketGrid
-                itemList={itemList}
-                setItems={setItems}
-            />
-            <div className="grand-total" >
-                <h2>GRAND TOTAL: {getTotal().toFixed(2) } {itemList[0]?.product.currency}</h2>
-            </div>
-
-            <div className="recurring-order">
-                <label>
-                    <h2>Monthly recurring order: <input type="checkbox" onChange={()=>changeRecurringOrder()}/></h2>
-
-                </label>
-            </div>
-
-            <div>
-                <ShowRebate
-                 showRebate={show.showRebate}
-                 product={show.product}
-                 pos={show.pos}
-                />
-            </div>
+                        </div>
+                        <div className="grid-item">
+                            <button className="unit-button" onClick={() => less(item)}>-</button>
+                            <a>{item.quantity}</a>
+                            <button className="unit-button" onClick={() => more(item)}>+</button>
+                        </div>
+                        <div className="grid-item">{(item.product.price * (1 - calculateRebate(item) * (1 / 100)) * item.quantity).toFixed(2)} {item.product.currency}</div>
+                        <div className="grid-item" style={{placeSelf:"center"}}>
+                            <label>
+                                <input type ="checkbox" onChange={()=>changeGiftWrapped(item)}/>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
+
 
 function ShowRebate(state: { showRebate:boolean; product?:Product;pos:{x:number;y:number} }){
     if(state.showRebate && state.product && state.product.rebatePercent===0)
@@ -244,5 +241,48 @@ type Product = {
 };
 type Item ={ product: Product; quantity: number; giftWrap: boolean };
 type Order = { itemList:Item[]; recurring: boolean };
+
+function Delivery() {
+
+    return (<div className="delivery-form">
+        <form>
+            <ul>
+                <li>
+                    <label key="name">Name: </label>
+                    <input type="text" id="name" name="name" placeholder="Name"/>
+                </li>
+                <li>
+                    <label key="phone">Phone: </label>
+                    <input type="tel" id="phone" name="phone" placeholder="00 00 00 00"/>
+                </li>
+                <li>
+                    <label key="email">E-mail: </label>
+                    <input type="email" id="email" name="email" placeholder="eksempel@eksempel.dk"/>
+                </li>
+                <li>
+                    <label key="address">Address: </label>
+                    <input type="text" id="address" name="address" placeholder="Address"/>
+                    <input type="text" id="address" name="address" placeholder="Address 2nd line"/>
+
+                </li>
+                <li>
+                    <label key="zip">Zip: </label>
+                    <input type="text" id="zip" name="zip" placeholder="Zip"/>
+                </li>
+                <li>
+                    <label key="city">City: </label>
+                    <input type="text" id="city" name="city" placeholder="City"/>
+                </li>
+                <li>
+                    <label key="country">Country: </label>
+                    <input type="text" id="country" name="country" placeholder="Country"/>
+                </li>
+                <li className="button">
+                    <button type="submit">Go to payment</button>
+                </li>
+            </ul>
+        </form>
+    </div>);
+}
 
 export default App
--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,13 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import React from "react";
-import App from "./App";
+import App from "../App";
 
 
 describe(App.name, () => {
     it("should render", () => {
         render(<App/>)
         expect(screen.getByText("GRAND TOTAL: 0.00")).toBeInTheDocument();
+    });
+    it("should render", () => {
+        render(<App/>)
+        expect(screen.getByText("Basket")).toBeInTheDocument();
     });
 });
 
