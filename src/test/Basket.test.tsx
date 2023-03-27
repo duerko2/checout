76d8559a29d53d24b8235d81c50755b183898b30
marcusjacobsen <S215799@dtu.dk>
2023-03-27 13:41:54 +0200

    tests bundled

--- src/test/Basket.test.tsx
+++ src/test/Basket.test.tsx
@@ -3,6 +3,7 @@ import {describe, expect, it, test} from "vitest";
 import React, {HTMLInputTypeAttribute} from "react";
 import userEvent from '@testing-library/user-event'
 import App from "../App";
+import {Basket} from "../Basket";
 
 
 function setup(jsx: React.ReactElement) {
@@ -12,76 +13,79 @@ function setup(jsx: React.ReactElement) {
     }
 }
 
-test('plus-minus item units', async () => {
-    const {user} = setup(<App/>)
+describe(Basket.name, () => {
+    test('plus-minus item units', async () => {
+        const {user} = setup(<App/>)
 
 
-    const minusButtons:HTMLElement[] = await screen.findAllByRole("button", {name: /-/i});
-    const plusButtons:HTMLElement[] = await screen.findAllByRole("button", {name: /\+/i});
+        const minusButtons: HTMLElement[] = await screen.findAllByRole("button", {name: /-/i});
+        const plusButtons: HTMLElement[] = await screen.findAllByRole("button", {name: /\+/i});
 
-    let units:HTMLParagraphElement[] = await screen.findAllByTitle("units");
+        let units: HTMLParagraphElement[] = await screen.findAllByTitle("units");
 
-    // Test plus button
-    expect(units[0].textContent).toBe("1");
-    await user.click(plusButtons[0]);
-    expect(units[0].textContent).toBe("2");
-
-    // Test minus
-    await user.click(minusButtons[0]);
-    await user.click(minusButtons[0]);
-    expect(units[0].textContent).toBe("0");
-
-    // Test min value for units (0)
-    await user.click(minusButtons[0]);
-    expect(units[0].textContent).toBe("0");
-
-    // test max value for units (100)
-    for (let i = 0; i < 102; i++) {
+        // Test plus button
+        expect(units[0].textContent).toBe("1");
         await user.click(plusButtons[0]);
-    }
-    expect(units[0].textContent).toBe("100");
+        expect(units[0].textContent).toBe("2");
+
+        // Test minus
+        await user.click(minusButtons[0]);
+        await user.click(minusButtons[0]);
+        expect(units[0].textContent).toBe("0");
+
+        // Test min value for units (0)
+        await user.click(minusButtons[0]);
+        expect(units[0].textContent).toBe("0");
+
+        // test max value for units (100)
+        for (let i = 0; i < 102; i++) {
+            await user.click(plusButtons[0]);
+        }
+        expect(units[0].textContent).toBe("100");
 
 
-
-})
+    })
 
 // test giftwrapped button
-test('giftwrapped button', async () => {
-    const {user} = setup(<App/>)
+    test('giftwrapped button', async () => {
+        const {user} = setup(<App/>)
 
-    const giftwrappedButtons:HTMLInputElement[] = await screen.findAllByTitle("giftwrapped");
+        const giftwrappedButtons: HTMLInputElement[] = await screen.findAllByTitle("giftwrapped");
 
-    expect(giftwrappedButtons[0].checked).toBe(false);
-    await user.click(giftwrappedButtons[0]);
-    expect(giftwrappedButtons[0].checked).toBe(true);
+        expect(giftwrappedButtons[0].checked).toBe(false);
+        await user.click(giftwrappedButtons[0]);
+        expect(giftwrappedButtons[0].checked).toBe(true);
 
-})
+    })
 
 // Test remove item
-test('remove item', async () => {
-    const {user} = setup(<App/>)
-    const firstItem:HTMLElement = (await screen.findAllByTitle("itemName"))[0];
-    const firstItemName= firstItem.textContent;
-
-    await user.click((await screen.findAllByTitle("removeItem"))[0]);
-
-    const items:HTMLElement[] = (await screen.findAllByTitle("itemName"));
+    test('remove item', async () => {
+        const {user} = setup(<App/>)
+        const firstItem: HTMLElement = (await screen.findAllByTitle("itemName"))[0];
+        const firstItemName = firstItem.textContent;
 
 
-    // Check the item has been removed from the list of items now displayed.
-    for(let item of items) {
-        expect(item.textContent).not.toBe(firstItemName);
-    }
+        // Remove the first item
+        await user.click((await screen.findAllByTitle("removeItem"))[0]);
 
-    // minus buttons
-    const minusButtons:HTMLInputElement[] = await screen.findAllByTitle("removeItem")
 
-    // remove all items
-    for (let i = 0; i < minusButtons.length ; i++) {
-        await user.click(minusButtons[0]);
-    }
+        // Get all items
+        const items: HTMLElement[] = (await screen.findAllByTitle("itemName"));
+        // Check the item has been removed from the list of items now displayed.
+        for (let item of items) {
+            expect(item.textContent).not.toBe(firstItemName);
+        }
 
-    // should state that there are no items in the basket
-    expect(screen.getByText("Basket is empty"
-    )).toBeInTheDocument();
+        // minus buttons
+        const minusButtons: HTMLInputElement[] = await screen.findAllByTitle("removeItem")
+
+        // remove all items
+        for (let i = 0; i < minusButtons.length; i++) {
+            await user.click(minusButtons[0]);
+        }
+
+        // should state that there are no items in the basket
+        expect(screen.getByText("Basket is empty"
+        )).toBeInTheDocument();
+    })
 })
