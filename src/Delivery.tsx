188a6c4f7ee08d5a3001a1423c42dac54cabb787
marcusjacobsen <S215799@dtu.dk>
2023-03-27 15:36:13 +0200

    form testssss

--- src/Delivery.tsx
+++ src/Delivery.tsx
@@ -55,101 +55,101 @@ export function Delivery({order,setOrder}:{order:{itemList:Item[],recurring:bool
             <h3>Delivery information</h3>
             <ul>
                 <li>
-                    <label key="name">Name: </label>
+                    <label htmlFor="name">Name: </label>
                     <input type="text" id="name" name="name" placeholder="Name" required={true}/>
                 </li>
                 <li>
-                    <label key="phone">Phone: </label>
+                    <label htmlFor="phone">Phone: </label>
                     <input type="text" id="tel" name="tel" pattern="[0-9]{8}" placeholder="0000000" required={true}
                            title="Please enter valid phone number"/>
                 </li>
                 <li>
-                    <label key="email">E-mail: </label>
+                    <label htmlFor="email">E-mail: </label>
                     <input type="email" id="email" name="email" placeholder="eksempel@eksempel.dk" required={true}/>
                 </li>
                 <li>
-                    <label key="address">Address: </label>
+                    <label htmlFor="address">Address: </label>
                     <input type="text" id="address" name="address" placeholder="Address" required={true}/>
                     <input type="text" id="address" name="address" placeholder="Address 2nd line "/>
                 </li>
                 <li>
-                    <label key="zip">Zip: </label>
+                    <label htmlFor="zip">Zip: </label>
                     <input type="text" id="zip" name="zip" placeholder="Zip" onChange={(e) => checkZip(e.target.value)}
                            pattern="[0-9]{4}" required={true}/>
                     <label id="validZip" style={{display: zipVisible ? "block" : "none"}}>Ikke en zip</label>
                 </li>
                 <li>
-                    <label key="city">City: </label>
+                    <label htmlFor="city">City: </label>
                     <input type="text" id="city" name="city" placeholder="City" readOnly={true} required={true} value={cityText.valueOf()}/>
                 </li>
                 <li>
-                    <label key="country">Country: </label>
+                    <label htmlFor="country">Country: </label>
                     <input type="text" id="country" name="country" value="Denmark" readOnly={true} required={true}/>
                 </li>
                 <li>
-                    <label key="Company Name">Company: </label>
+                    <label htmlFor="Company Name">Company: </label>
                     <input type="text" id="company" name="company" placeholder="Company"/>
                 </li>
                 <li>
-                    <label key="VAT">VAT: </label>
+                    <label htmlFor="VAT">VAT: </label>
                     <input type="text" id="vat" name="vat" placeholder="00000000" pattern={"[0-9]{8}"}/>
                 </li>
                 <li className="accept-condition">
                     <input type="checkbox" id="checkbox" onChange={()=>setSeparateBilling(!separateBilling)}/>
-                    <label key="seperatebilling">Seperate Billing Address: </label>
+                    <label htmlFor="seperatebilling">Seperate Billing Address: </label>
                 </li>
                 <div style={{display: separateBilling ? "block" : "none"}}>
                     <h3>Billing Address</h3>
                     <li>
-                        <label key="billingname">Name: </label>
+                        <label htmlFor="billingname">Name: </label>
                         <input type="text" id="name" name="billingname" placeholder="Name" required={true}/>
                     </li>
                     <li>
-                        <label key="billingphone">Phone: </label>
+                        <label htmlFor="billingphone">Phone: </label>
                         <input type="text" id="tel" name="billingphone" pattern="[0-9]{8}" placeholder="0000000" required={true} title="Please enter valid phone number"/>
                     </li>
                     <li>
-                        <label key="billingemail">E-mail: </label>
+                        <label htmlFor="billingemail">E-mail: </label>
                         <input type="email" id="email" name="billingemail" placeholder="eksempel@eksempel.dk" required={true}/>
                     </li>
                     <li>
-                        <label key="billingaddress">Address: </label>
+                        <label htmlFor="billingaddress">Address: </label>
                         <input type="text" id="address" name="billingaddress" placeholder="Address" required={true}/>
                         <input type="text" id="address" name="billingaddress" placeholder="Address 2nd line "/>
                     </li>
                     <li>
-                        <label key="billingzip">Zip: </label>
+                        <label htmlFor="billingzip">Zip: </label>
                         <input type="text" id="zip" name="billingzip" placeholder="Zip" onChange={(e) => checkZip(e.target.value)}
                                pattern="[0-9]{4}" required={true}/>
                         <label id="validZip">Ikke en zip</label>
                     </li>
                     <li>
-                        <label key="billingcity">City: </label>
+                        <label htmlFor="billingcity">City: </label>
                         <input type="text" id="city" name="billingcity" placeholder="City" readOnly={true} required={true}/>
                     </li>
                     <li>
-                        <label key="billingcountry">Country: </label>
+                        <label htmlFor="billingcountry">Country: </label>
                         <input type="text" id="country" name="billingcountry" value="Denmark" readOnly={true} required={true}/>
                     </li>
                     <li>
-                        <label key="billingCompanyName">Company: </label>
+                        <label htmlFor="billingCompanyName">Company: </label>
                         <input type="text" id="company" name="billingCompanyName" placeholder="Company"/>
                     </li>
                     <li>
-                        <label key="BillingVAT">VAT: </label>
+                        <label htmlFor="BillingVAT">VAT: </label>
                         <input type="text" id="vat" name="BillingVAT" placeholder="00000000" pattern={"[0-9]{8}"}/>
                     </li>
                 </div>
                 <li className="accept-condition" style={{marginTop: "1em"}}>
                     <input name="terms&conditions" type="checkbox" id="checkbox" required={true}/>
-                    <label key="terms&conditions">I accept terms & conditions</label>
+                    <label htmlFor="terms&conditions">I accept terms & conditions</label>
                 </li>
                 <li className="accept-condition">
                     <input name="marketingemails" type="checkbox" id="checkbox"/>
-                    <label key="marketingemails">I accept to receive marketing emails</label>
+                    <label htmlFor="marketingemails">I accept to receive marketing emails</label>
                 </li>
                 <li>
-                    <label key="comment">Comment</label>
+                    <label htmlFor="comment">Comment</label>
                     <textarea name="comment" rows={4}>
                     </textarea>
                 </li>
