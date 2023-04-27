4cf716022feb8fd3b353f8cf1907bcfa83735ddc
Alex Batten <alex.batten@live.com>
2023-04-27 23:01:00 +0200

    payment form changes

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
         function simulateLoading() {
             setTimeout(() => {
                 setIsLoading(false);
-            }, 1500);
+            }, 1200);
         }
 
         simulateLoading();
